ca31f9598765f9a65923aac57630f5d0aea4e558
Narkisin <oscar8882901@gmail.com>
2025-06-11 17:21:20 -0300

    fix: agrego CORS al backend

--- src/app/api/verificar/route.js
+++ src/app/api/verificar/route.js
@@ -1,5 +1,16 @@
 let fingerprints = new Set();
 
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // O reemplazá con tu dominio exacto (ej. "http://127.0.0.1:5501" o "https://arbitrade.lat")
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+    }
+  });
+}
+
 export async function POST(req) {
   const { fingerprint } = await req.json();
   const isRepeated = fingerprints.has(fingerprint);
@@ -8,18 +19,7 @@ export async function POST(req) {
   return new Response(JSON.stringify({ status: isRepeated ? "repetido" : "ok" }), {
     status: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*", // o poné tu dominio exacto aquí si querés restringir
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-    }
-  });
-}
-
-export async function OPTIONS() {
-  return new Response(null, {
-    status: 204,
-    headers: {
-      "Access-Control-Allow-Origin": "https://arbitrade.lat",
+      "Access-Control-Allow-Origin": "*", // mismo dominio o "*"
       "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
     }
