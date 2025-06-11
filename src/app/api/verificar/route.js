5f564127a620252ae0718316c11f2157a16d819a
Narkisin <oscar8882901@gmail.com>
2025-06-11 18:02:08 -0300

    fix: agrego CORS al backend

--- src/app/api/verificar/route.js
+++ src/app/api/verificar/route.js
@@ -1,25 +1,35 @@
-let fingerprints = new Set();
-
 export async function OPTIONS() {
   return new Response(null, {
     status: 204,
     headers: {
-      "Access-Control-Allow-Origin": "*", // O reemplazá con tu dominio exacto (ej. "http://127.0.0.1:5501" o "https://arbitrade.lat")
+      "Access-Control-Allow-Origin": "*", // permite desde cualquier origen
       "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
     }
   });
 }
 
-export async function POST(req) {
-  const { fingerprint } = await req.json();
-  const isRepeated = fingerprints.has(fingerprint);
-  if (!isRepeated) fingerprints.add(fingerprint);
-
-  return new Response(JSON.stringify({ status: isRepeated ? "repetido" : "ok" }), {
+export async function GET() {
+  return new Response(JSON.stringify({
+    meta_pixel_id: "1220554166264708",
+    token: "224a105d0c07616da76d0262438e75cb", // tu token completo
+    email: "oscar882901@gmail.com",
+    mensaje_wpp: "Hola! Vengo de arbitrade",
+    dominio: "arbitrade.lat",
+    numeros: [
+      "5491171464849",
+      "5491171756767",
+      "5491171464592",
+      "5491127455503",
+      "5491171465461",
+      "5491171501589",
+      "5491171489956",
+      "5491176298293"
+    ]
+  }), {
     status: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*", // mismo dominio o "*"
+      "Access-Control-Allow-Origin": "*", // permite desde todos los orígenes
       "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
     }
